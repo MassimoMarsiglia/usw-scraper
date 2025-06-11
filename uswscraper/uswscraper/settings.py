5c49072c52e5e43ac21b08b83d23049d2f90669e
Massimo Marsiglia <marsigliamassi@gmail.com>
2025-06-11 18:33:50 +0200

    setup proxy middleware

--- uswscraper/uswscraper/settings.py
+++ uswscraper/uswscraper/settings.py
@@ -52,9 +52,13 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "uswscraper.middlewares.UswscraperDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_proxies.RandomProxy': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+}
+
+PROXY_LIST = "./proxies.txt"
+PROXY_MODE = 0
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
---
+++ uswscraper/uswscraper/spiders/example.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class ExampleSpider(scrapy.Spider):
+    name = "example"
+    allowed_domains = ["httpbin.org"]
+    start_urls = ["https://httpbin.org/ip"] * 100  # Simulating multiple requests
+    
+
+    def parse(self, response):
+        # Log the proxy used for this request
+        proxy_used = response.request.meta.get('proxy', 'No proxy')
+        self.logger.info(f"Response from {response.url} using proxy: {proxy_used}")
+        
+        # Parse the JSON response to show the IP being used
+        try:
+            data = response.json()
+            ip = data.get('origin', 'Unknown IP')
+            self.logger.info(f"Current IP: {ip}")
+            yield {'url': response.url, 'ip': ip, 'proxy': proxy_used}
+        except:
+            self.logger.error(f"Failed to parse JSON response from {response.url}")
